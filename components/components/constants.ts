9398c346903bc2af062ce3207cbe314495e13de4
Techbrolakes <lekandar11@gmail.com>
2024-03-23 22:53:59 +0100

    feat: Initial Project Structure

--- components/components/constants.ts
+++
@@ -1,8 +0,0 @@
-const PAGE_HEAD = {
-    APP_NAME: 'Courseplug',
-    PAGE_DESC:
-        'Courseplug is your ultimate solultion to getting access to premium tech udemy courses from top instructors',
-    PAGE_KEYWORDS: ['https://courseplug.xyz/'],
-};
-
-export default PAGE_HEAD;
