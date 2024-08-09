24ea022c3089867d079578cd9479227a58889974
Olamilekan Daramola <45468437+Techbrolakes@users.noreply.github.com>
2024-08-09 08:32:49 +0100

    feat: prod push

--- source/config/development.config.ts
+++ source/config/development.config.ts
@@ -4,8 +4,8 @@ import Config from "./config";
 
 const config: Config = {
   env: "development",
-  // database: "mongodb+srv://bdaiveed:neew2023@neewcluster.dzzdlnv.mongodb.net/?retryWrites=true&w=majority&appName=NeewCluster",
-  database: "mongodb+srv://lekandar:helloworld@neew-cluster.5wiort4.mongodb.net/",
+  database: "mongodb+srv://bdaiveed:neew2023@neewcluster.dzzdlnv.mongodb.net/?retryWrites=true&w=majority&appName=NeewCluster",
+  // database: "mongodb+srv://lekandar:helloworld@neew-cluster.5wiort4.mongodb.net/",
   databaseName: "neew-database",
   frontUrl: "https://neew-frontend-app.vercel.app/",
   imageFolder: path.join(
---
+++ source/models/company.model.ts
@@ -0,0 +1,57 @@
+import mongoose, { Schema } from "mongoose";
+
+export interface ICompany {
+  traction: string;
+  usp: string;
+  website: string;
+  productMaturity: string;
+  revenue: string;
+  sdgImpact: string[];
+  operatingAddress: string;
+  communityImpact: string;
+  companyLogo: string;
+  companyName: string;
+  ebitda: string;
+  exitFounder: string;
+  exitMethod: string;
+  exitStrategy: string;
+  exitTimeFrame: string;
+  forecastedEbitda: string;
+  founderValuation: string;
+  founderValuationLogic: string;
+  foundingYear: string;
+  growthTrend: string;
+  locationOfRegistration: string;
+  networkWants: string;
+}
+
+const CompanySchema = new mongoose.Schema({
+  traction: { required: false, type: String },
+  usp: { required: false, type: String },
+  website: { required: false, type: String },
+  productMaturity: { required: false, type: String },
+  revenue: { required: false, type: String },
+  sdgImpact: { default: [], type: [String] },
+  operatingAddress: { required: false, type: String },
+  communityImpact: { required: false, type: String },
+  companyLogo: { required: false, type: String },
+  companyName: { required: false, type: String },
+  ebitda: { required: false, type: String },
+  exitFounder: { required: false, type: String },
+  exitMethod: { required: false, type: String },
+  exitStrategy: { required: false, type: String },
+  exitTimeFrame: { required: false, type: String },
+  forecastedEbitda: { required: false, type: String },
+  founderValuation: { required: false, type: String },
+  founderValuationLogic: { required: false, type: String },
+  foundingYear: { required: false, type: String },
+  growthTrend: { required: false, type: String },
+  locationOfRegistration: { required: false, type: String },
+  networkWants: { required: false, type: String },
+});
+
+// A document type combining ICompany and Document interfaces
+export interface ICompanyDocument extends mongoose.Document, ICompany {}
+
+// A model type combining ICompanyDocument and Model interfaces
+export const CompanyModel = mongoose.model<ICompanyDocument>("Company", CompanySchema);
